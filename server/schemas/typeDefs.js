66b432076e2af9d79250391a1b6b7025e56e7668
Peter Tran <pettran866@gmail.com>
2022-08-18 10:49:15 -0700

    create User typeDef

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,15 @@
+// import the gql tagged template function
+const { gql } = require('apollo-server-express');
+
+// create our typeDefs
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    patterns: [Pattern]
+  },
+`;
+
+// export the typeDefs
+module.exports = typeDefs;
