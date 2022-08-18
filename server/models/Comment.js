8ec8855d8c5f020028735c4c575d9a836568e4ca
avandefeniks <angel.vandefeniks@gmail.com>
2022-08-18 11:51:56 -0700

    server.js, config.js, dateFormat helper and models completed.

---
+++ server/models/Comment.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const commentSchema = new Schema(
+    {
+        commentText: {
+            type: String,
+            required: 'You need to leave a comment!',
+            minlength: 1,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: timestamp => dateFormat(timestamp)
+          },
+          username: {
+            type: String,
+            required: true
+          }
+    }
+);
+
+const Comment = model('Comment', commentSchema);
+
+module.exports = Comment;
---
+++ server/models/Pattern.js
@@ -0,0 +1,56 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const patternSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        username: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        project: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        for: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        skill: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        comment: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Comment'
+            }
+        ],
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: timestamp => dateFormat(timestamp)
+        },
+        yarn: {
+            type: Schema.Types.ObjectId,
+            ref: 'Yarn'
+        },
+        yarn: {
+            type: Schema.Types.ObjectId,
+            ref: 'needle'
+        },
+
+    }
+);
+
+const Pattern = model('Pattern', patternSchema);
+
+module.exports = Pattern;
