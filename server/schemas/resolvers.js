08b50b83424f3356dc1a868ae04468b77817fdc6
Peter Tran <pettran866@gmail.com>
2022-08-18 18:41:12 -0700

    added login and addUser mutations to resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -48,5 +48,31 @@ const resolvers = {
     needle: async () => {
       return Needle.find();
     } 
+  },
+
+  Mutation: {
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+    
+      const correctPw = await user.isCorrectPassword(password);
+    
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+      
+      const token = signToken(user);
+      return { token, user };
+    },
+
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
   }
 }
