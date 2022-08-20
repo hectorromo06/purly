04563caee5c297c22715f16f70e2ba1fb0b33b60
avandefeniks <angel.vandefeniks@gmail.com>
2022-08-20 15:21:00 -0700

    added queries for yarn characteristics including weigh, color and fiber

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -14,11 +14,23 @@ query user($username: String!) {
       for
       skill
       createdAt
-      fiber
-      weight
-      color
       description
       instructions
+      fiber {
+        _id
+        type
+        name
+      }
+      weight {
+        _id
+        type
+        name
+      }
+      color {
+        _id
+        type
+        name
+      }
       needle {
         _id
         size 
@@ -42,18 +54,30 @@ export const QUERY_ME = gql`
     username
     email
     patterns {
-      _id
+        _id
       name
       username
       project
       for
       skill
       createdAt
-      fiber
-      weight
-      color
       description
       instructions
+      fiber {
+        _id
+        type
+        name
+      }
+      weight {
+        _id
+        type
+        name
+      }
+      color {
+        _id
+        type
+        name
+      }
       needle {
         _id
         size 
@@ -74,21 +98,39 @@ export const QUERY_PATTERNS = gql`
 query user($username: String!) {
     user(username: $username) {
     patterns {
-      _id
+        _id
       name
       username
       project
       for
       skill
       createdAt
-      fiber
-      weight
-      color
       description
       instructions
+      fiber {
+        _id
+        type
+        name
+      }
+      weight {
+        _id
+        type
+        name
+      }
+      color {
+        _id
+        type
+        name
+      }
       needle {
         _id
         size 
+      }
+      comments {
+        _id
+        commentText
+        username
+        createdAt
       }
       
     }
@@ -100,5 +142,32 @@ export const QUERY_NEEDLES = gql`
 query needle {
     _id
     size
+  }
+`;
+
+export const QUERY_YARN_WEIGHT = gql`
+query yarnCharacteristic($type: String!) {
+    yarnCharacteristic(type: $type)
+    _id
+    type
+    name
+  }
+`;
+
+export const QUERY_YARN_COLOR = gql`
+query yarnCharacteristic($type: String!) {
+    yarnCharacteristic(type: $type)
+    _id
+    type
+    name
+  }
+`;
+
+export const QUERY_YARN_FIBER = gql`
+query yarnCharacteristic($type: String!) {
+    yarnCharacteristic(type: $type)
+    _id
+    type
+    name
   }
 `;
