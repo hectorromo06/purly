1dbd691aaa99010e231c2b54f4120ebb608df8c0
avandefeniks <angel.vandefeniks@gmail.com>
2022-08-20 18:46:04 -0700

    client queries and mutations complete

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -12,3 +12,68 @@ export const ADD_USER = gql`
   }
 `;
 
+export const LOGIN_USER = gql`
+  mutation addUser($email: String!, $password: String!) {
+    addUser(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_COMMENT = gql`
+mutation addComment($patternId: ID!, $commentText: String!) {
+    addComment(patternId: $patternId, commentText: $commentText) {
+      comments {
+        commentText
+        username
+    }
+    _id
+    name
+    project
+    for
+    skill
+    username
+    description
+    createdAt
+  }
+}
+`;
+
+export const ADD_PATTERN = gql`
+mutation addPattern($input: PatternInput!) {
+    addPattern(input: $input){
+    _id
+    name
+    project
+    for
+    skill
+    fiber {
+      _id
+      type
+      name
+    }
+    weight {
+      _id
+      type
+      name
+    }
+    color {
+      _id
+      type
+      name
+    }
+    needle {
+      _id
+      size
+    }
+    username
+    description
+    instructions
+  }
+}
+`;
+
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -14,34 +14,7 @@ query user($username: String!) {
       for
       skill
       createdAt
-      description
-      instructions
-      fiber {
-        _id
-        type
-        name
-      }
-      weight {
-        _id
-        type
-        name
-      }
-      color {
-        _id
-        type
-        name
-      }
-      needle {
-        _id
-        size 
-      }
-      comments {
-        _id
-        commentText
-        username
-        createdAt
-      }
-      
+      description      
     }
   }
 }
@@ -54,86 +27,56 @@ export const QUERY_ME = gql`
     username
     email
     patterns {
-        _id
+      _id
       name
       username
       project
       for
       skill
       createdAt
-      description
-      instructions
-      fiber {
-        _id
-        type
-        name
-      }
-      weight {
-        _id
-        type
-        name
-      }
-      color {
-        _id
-        type
-        name
-      }
-      needle {
-        _id
-        size 
-      }
-      comments {
-        _id
-        commentText
-        username
-        createdAt
-      }
-      
+      description      
     }
   }
 }
 `;
 
 export const QUERY_PATTERNS = gql`
-query user($username: String!) {
-    user(username: $username) {
-    patterns {
-        _id
-      name
+query pattern($username: String) {
+    patterns(username: $username) {
+    _id
+    name
+    project
+    for
+    skill
+    comments {
+      _id
+      commentText
       username
-      project
-      for
-      skill
       createdAt
-      description
-      instructions
-      fiber {
-        _id
-        type
-        name
-      }
-      weight {
-        _id
-        type
-        name
-      }
-      color {
-        _id
-        type
-        name
-      }
-      needle {
-        _id
-        size 
-      }
-      comments {
-        _id
-        commentText
-        username
-        createdAt
-      }
-      
     }
+    createdAt
+    fiber {
+      _id
+      type
+      name
+    }
+    weight {
+      _id
+      type
+      name
+    }
+    color {
+      _id
+      type
+      name
+    }
+    needle {
+      _id
+      size
+    }
+    username
+    description
+    instructions
   }
 }
 `;
@@ -146,7 +89,7 @@ query needle {
 `;
 
 export const QUERY_YARN_WEIGHT = gql`
-query yarnCharacteristic($type: String!) {
+query yarnCharacteristic($type: String) {
     yarnCharacteristic(type: $type)
     _id
     type
@@ -155,7 +98,7 @@ query yarnCharacteristic($type: String!) {
 `;
 
 export const QUERY_YARN_COLOR = gql`
-query yarnCharacteristic($type: String!) {
+query yarnCharacteristic($type: String) {
     yarnCharacteristic(type: $type)
     _id
     type
@@ -164,7 +107,7 @@ query yarnCharacteristic($type: String!) {
 `;
 
 export const QUERY_YARN_FIBER = gql`
-query yarnCharacteristic($type: String!) {
+query yarnCharacteristic($type: String) {
     yarnCharacteristic(type: $type)
     _id
     type
