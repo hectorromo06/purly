a2a39f00bf185d91ab17415a1b198f41546e0134
analizajb <96861913+analizajb@users.noreply.github.com>
2022-08-22 17:10:06 -0700

    updating App.js to include Login / including utils/auth.js and logic

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,29 @@
 import React from "React";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
   createHttpLink,
-} from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
 // import Header from './components/Header';
 // import Footer from './components/Footer';
 
-import Signup from './pages/Signup';
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  uri: "/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('id_token');
+  const token = localStorage.getItem("id_token");
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : '',
+      authorization: token ? `Bearer ${token}` : "",
     },
   };
 });
@@ -36,12 +37,12 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <Router>
-       <Routes>
-              <Route 
-                path="/signup" 
-                element={<Signup />} 
-              />
-            </Routes>
+        <div>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+        </div>
       </Router>
     </ApolloProvider>
   );
