2d82313f329922c7546bc29c62d1df0eff0b001f
Quez <alinamvelazquez@gmail.com>
2022-08-23 19:35:19 -0700

    changed title to header

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Title from './Title.js';
+import Header from './Header.js';
 
 function App() {
   return (
--- client/src/Header.js
+++ client/src/Header.js
@@ -1,10 +1,11 @@
 import React from "react";
-import 'Title.css';
+import 'Header.css';
 
 function Title() {
     return (
         <div className="Title">
-            <h1>TEST</h1>
+            <h1>PURLY</h1>
+            <h2>Let's Make Something!</h2>
         </div>
     )
 }
