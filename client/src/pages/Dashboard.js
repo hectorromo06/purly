21cbf7165315445065e99ba35eb0a29264ea61e5
jbairoso <jbairoso@gmail.com>
2022-08-24 21:15:28 -0700

    redo dashboard to show a single users profile

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,54 +1,57 @@
 import React from "react";
-// import {Link} from
+import "./Login.js";
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/esm/Container";
+import { Link } from "react-router-dom";
 import { QUERY_ME } from "../../src/utils/queries";
 import { useQuery } from "@apollo/client";
 
-export default function Dashboard() {
+export default function UserDashboard() {
   const { data, loading } = useQuery(QUERY_ME);
 
-  let allUsers;
+  let cardInfo;
+
+  let username;
 
   if (!loading) {
-    allUsers = data.QUERY_ME;
+    username = data.QUERY_ME.username;
+    cardInfo = data.QUERY_ME.card;
   } else {
     return <h2>loading...</h2>;
   }
 
-  const renderAllUsers = (users, index) => {
-    return users.card.map((card, index) => {
-      return (
-        <Container className="col-auto">
-          <Card
-            style={{ width: "18rem", height: "30rem" }}
-            key={card._id}
-            className="shadow-lg m-1 mb-5 text-center"
-          >
-            <Card.Img variant="top" src={card.image} alt="context img" />
-            <Card.Body>
-              <Card.Title className="cardUsername pb-2">
-                {users.username}
-              </Card.Title>
-              <p>{card.description}</p>
-              <p className="mb-0">Level</p>{" "}
-              <h1 className="mb-0 test">${card.level}</h1>
-              <button className="mt-4 btn btn-success">
-                <Link className="text-decoration-none text-white" to="/pattern">
-                  Pattern
-                </Link>
-              </button>
-            </Card.Body>
-          </Card>
-        </Container>
-      );
-    });
-  };
-
   return (
     <>
-      <div className="container d-flex flex-wrap">
-        {allUsers.map(renderAllUsers)}
+      <div>
+        <div className="cardContainer text-center">
+          <h1 className="">Your Creative Dasboard</h1>
+        </div>
+
+        <div className="d-flex flex-wrap mt-5 container">
+          {cardInfo.map((card, index) => {
+            return (
+              <Container className="col-auto">
+                <Card
+                  style={{ width: "18rem", height: "30rem" }}
+                  key={index}
+                  className="shadow-lg m-1 mb-5 text-center"
+                >
+                  <Card.Img variant="top" src={card.image} />
+
+                  <Card.Body>
+                    <Card.Title className="cardUsername pb-2">
+                      {username}
+                    </Card.Title>
+                    <p>{card.description}</p>
+                    <p>{card.email}</p>
+                    <p>{card.patterns}</p>
+                  </Card.Body>
+                </Card>
+              </Container>
+            );
+          })}
+        </div>
       </div>
-      <Footer/>
     </>
   );
 }
