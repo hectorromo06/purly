0846d92beaad2865e2c8eed532e0b9f829cb0e16
Hector Romo <hectorommmo@gmail.com>
2022-08-24 19:50:47 -0700

    home page skeleton

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 
+import React, { useState } from 'react';
+// import Dashboard from './components/Dashboard';
+import Header from './components/Header';
+// import Account from './components/Account';
+
+
+
 function App() {
+  // nav options
+  const [navOptions] = useState(['My Account', 'My Projects', 'Patterns']);
+
+  const [currentNavOp] = useState(navOptions[0]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header 
+       navOptions={navOptions}
+       currentNavOp={currentNavOp}
+      />
+       {/* {
+        currentNavOp === 'My Account' &&
+        <Account />
+       } */}
+
+
     </div>
   );
 }
