db24556b9f41415fed042c8aa26e1929501db33a
analizajb <96861913+analizajb@users.noreply.github.com>
2022-08-27 18:34:40 -0700

    inputting Comments component and SinglePattern page to include Comments

--- client/src/pages/SinglePattern.js
+++ client/src/pages/SinglePattern.js
@@ -1,21 +1,51 @@
 import React from 'react';
 
-import { useQuery } from '@apollo/client';
-
+import { useMutation, useQuery } from '@apollo/client';
+import { QUERY_USER, QUERY_ME } from '../utils/queries';
 import { QUERY_PATTERN } from '../utils/queries';
-import { useParams } from 'react-router-dom';
+import { ADD_COMMENT } from '../utils/mutations';
+import { Navigate, useParams } from 'react-router-dom';
+import CommentForm from '../components/Comments';
+import Auth from '../utils/auth';
 
 const SinglePattern = (props) => {
+    const { username: userParam } = useParams();
+    const [addComment] = useMutation(ADD_COMMENT);
     const { id: patternId } = useParams();
-    const { loading, data } = useQuery(QUERY_PATTERN, {
-        variables: { _id: patternId },
+    const { loading, data } = useQuery(QUERY_PATTERN, userParam ? QUERY_USER : QUERY_ME , {
+        variables: { _id: patternId, username: userParam },
       });
-
+    
+    const user = data?.me || data?.user || {};
     const pattern = data?.pattern || {};
 
+    // navigate to pattern page if username is yours
+    if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
+    return <Navigate to="/pattern/:id/user" />;
+    }
     if (loading) {
      return <div>Loading...</div>;
     }
+  
+
+    // if (!user?.username) {
+    //   return (
+    //     <h4>
+    //       You need to be logged in to see this. Use the navigation links above to
+    //       sign up or log in!
+    //     </h4>
+    //   );
+    // }
+
+    const handleClick = async () => {
+      try {
+        await addComment({
+          variables: { id: user._id },
+        });
+      } catch (e) {
+        console.error(e);
+      }
+    };
 
     // Image tag should be added soon
  return (
@@ -37,9 +67,21 @@ const SinglePattern = (props) => {
           <p>{pattern.instructions}</p>
         </div>
       </div>
-    </div>
+      
+      {/* COMMENTS */}
+      <div className="comment-content">
+        <h2 className="">
+          Viewing {userParam ? `${user.username}'s` : 'your'} profile.
+        </h2>
+        <CommentForm />
 
-    // ADD COMMENT FUNCTIONALITY
+        {userParam && (
+          <button className="add-comment" onClick={handleClick}>
+            Add Comment
+          </button>
+        )}
+      </div>
+    </div>
   );
 }
 
